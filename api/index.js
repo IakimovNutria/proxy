81a6317b816f18b0291719efd280e3b5c56048e2
Iakimov Sergey <serg.n.y@yandex.ru>
2023-05-20 13:53:30 +0300

    cookie

--- api/index.js
+++ api/index.js
@@ -3,5 +3,14 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-app.use('/', createProxyMiddleware({ target: 'http://51.250.72.142:1337', changeOrigin: true }));
+app.use('/', createProxyMiddleware({
+    target: 'http://51.250.72.142:1337',
+    changeOrigin: true,
+    onProxyRes: function(proxyRes, req, res) {
+        const cookies = proxyRes.headers['set-cookie'];
+        if (cookies) {
+            res.setHeader('Set-Cookie', cookies);
+        }
+    }
+}));
 app.listen(3000);
