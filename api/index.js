2d30fadb345127a74219c2ebd4292d9e85bbc37d
Iakimov Sergey <serg.n.y@yandex.ru>
2023-05-20 13:59:31 +0300

    fix

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ app.use('/', createProxyMiddleware({
     changeOrigin: true,
     onProxyRes: function(proxyRes, req, res) {
         const cookies = proxyRes.headers['set-cookie'];
-        console.log(cookies);
+        cookies.sameSite = 'undefined';
         if (cookies) {
             res.setHeader('Set-Cookie', cookies);
         }
