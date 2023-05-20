1a7d26e07d7eb76b205990e1b186197f9ed88593
Iakimov Sergey <serg.n.y@yandex.ru>
2023-05-20 20:02:43 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,6 +5,14 @@ const app = express();
 
 app.use('/', createProxyMiddleware({
     target: 'http://51.250.72.142:1337',
-    changeOrigin: true
+    changeOrigin: true,
+    onProxyRes: function(proxyRes, req, res) {
+        const cookies = proxyRes.headers['set-cookie'];
+        if (cookies) {
+            const newCookies = cookies.map(cookie => cookie.replace('SameSite=Lax', 'SameSite=None;Secure'));
+            res.setHeader('Set-Cookie', newCookies);
+        }
+    }
+
 }));
 app.listen(3000);
